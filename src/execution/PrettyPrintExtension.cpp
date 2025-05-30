44aec4b075e698be23689ecedbf74b008fcd6638
Christian20003 <christian.goellner@bnv-bamberg.de>
2025-05-30 09:41:47 +0200

    Increase output size so that every row will be printed. Fix printing bug with bfloat extension

--- src/execution/PrettyPrintExtension.cpp
+++ src/execution/PrettyPrintExtension.cpp
@@ -3,12 +3,19 @@
 
 arrow::Status lingodb::execution::ExtensionPrinter::PrettyPrint(const arrow::ChunkedArray& chunked_arr, std::ostream* sink) {
     this->sink = sink;
+    *(this->sink) << "[";
     for (int i = 0; i < chunked_arr.num_chunks(); ++i) {
-        *(this->sink) << "[\n[\n";
+        *(this->sink) << "\n[";
         auto chunk = chunked_arr.chunk(i);
         ARROW_RETURN_NOT_OK(arrow::VisitArrayInline(*chunk, this));
-        *(this->sink) << "\n]\n]";
+        if (i + 1 == chunked_arr.num_chunks()) {
+            *(this->sink) << "]";
+        } else {
+            *(this->sink) << "],";
+        }
+        
     }
+    *(this->sink) << "\n]";
     return arrow::Status::OK();
 }
 
@@ -18,9 +25,12 @@ arrow::Status lingodb::execution::ExtensionPrinter::Visit(const arrow::HalfFloat
     for (std::optional<typename arrow::HalfFloatType::c_type> value : array) {
        if (value.has_value()) {
           halfFloatValue.asInt = static_cast<unsigned>(value.value()) << 16;
+          *sink << "\n";
           *sink << halfFloatValue.asFloat;
           if (index < array.length() - 1) {
-             *sink << ",\n";
+             *sink << ",";
+          } else {
+             *sink << "\n";
           }
           ++index;
        }
